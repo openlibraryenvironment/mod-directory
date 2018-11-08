718d5671bf2404bcbca2b3fcf8cb4cc1e21943e2
Ian Ibbotson <ianibbo@googlemail.com>
2018-11-08 15:46:33 +0000

    Added Dirent

---
+++ service/grails-app/domain/org/olf/okapi/modules/directory/DirectoryEntry.groovy
@@ -0,0 +1,22 @@
+package org.olf.okapi.modules.directory
+
+class DirectoryEntry {
+
+  String id
+  String name
+  String slug
+  String description
+
+  static mapping = {
+                 id column:'de_id', generator: 'uuid', length:36
+               name column:'de_name'
+               slug column:'de_slug'
+        description column:'de_desc'
+  }
+
+  static constraints = {
+           name(nullable:false, blank:false)
+           slug(nullable:true, blank:false)
+    description(nullable:true, blank:false)
+  }
+}
---
+++ service/src/test/groovy/org/olf/okapi/modules/directory/DirectoryEntrySpec.groovy
@@ -0,0 +1,18 @@
+package org.olf.okapi.modules.directory
+
+import grails.testing.gorm.DomainUnitTest
+import spock.lang.Specification
+
+class DirectoryEntrySpec extends Specification implements DomainUnitTest<DirectoryEntry> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
