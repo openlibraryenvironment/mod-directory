8ab180f5bb2f8216262520c48b7c7654fc76b344
Ian Ibbotson <ianibbo@gmail.com>
2019-01-27 22:45:10 +0000

    Updated test2

--- scripts/test2.sh
+++ scripts/test2.sh
@@ -1,7 +1,9 @@
 AUTH_TOKEN=`./okapi-login`
 
+echo Got auth token $AUTH_TOKEN
+
 echo Listing current entries
-curl --header "X-Okapi-Tenant: diku" -H "X-Okapi-Token: ${AUTH_TOKEN}" -H "Content-Type: application/json" -X GET http://localhost:9130/directory/entry 
+curl --header "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $AUTH_TOKEN" -H "Content-Type: application/json" -X GET http://localhost:9130/directory/entry 
 
 # create the url custom property
 echo
@@ -9,7 +11,7 @@ echo Create url custom property
 curl -sSL -H 'Accept:application/json' \
 	  -H 'Content-Type: application/json' \
 	  -H 'X-OKAPI-TENANT: diku' \
-	  -H 'X-Okapi-Token: ${AUTH_TOKEN}' \
+	  -H "X-Okapi-Token: $AUTH_TOKEN" \
 	  -X POST http://localhost:9130/directory/custprops -d '{
  "name":"url", 
  "type":"Text"
@@ -17,19 +19,32 @@ curl -sSL -H 'Accept:application/json' \
 
 echo
 echo Create iso-18626 loopback service
-DirEnt1=`curl -sSL -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: ${AUTH_TOKEN}" -H "Content-Type: application/json" -X POST http://localhost:9130/directory/entry -d ' {
+DirEnt1=`curl -sSL -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $AUTH_TOKEN" -H "Content-Type: application/json" -X POST http://localhost:9130/directory/entry -d ' {
+  "name":"diku",
+  "slug":"diku",
+  "description":"diku",
+  "tags":[
+    "Testdata", "TestRun1", "MonographTest"
+  ],
+  "customProperties":{
+    "url": ["http://localhost:9130/rs/iso18626"]
+  }
+}'`
+
+echo
+echo Created request 1: $DirEnt1
+
+service_1=`curl -sSL -H "X-Okapi-Tenant: diku" -H "X-Okapi-Token: $AUTH_TOKEN" -H "Content-Type: application/json" -X POST http://localhost:9130/directory/service -d ' {
   name:"loopback-iso-18626",
-  slug:"loopback-iso-18626",
-  description:"A loopback interface for the local ISO18626 edge API",
+  type:"ISO18626",
   tags:[
     "Testdata", "TestRun1", "MonographTest"
   ],
   customProperties:{
-    "url": ["http://localhost:9130/rs/iso18626"],
+    "url": ["http://localhost:9130/rs/iso18626"]
   }
 }
 '`
-
 echo
-echo Created request 1: $DirEnt1
+echo Created service 1: $service_1
 
