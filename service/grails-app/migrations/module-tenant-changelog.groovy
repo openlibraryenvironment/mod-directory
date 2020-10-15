1477da8875c859737e7edb5fd7c7114a71ae81fb
Ian Ibbotson <ianibbo@gmail.com>
2020-10-15 16:38:31 +0100

    Move source sets up in build.gradle

--- service/grails-app/migrations/module-tenant-changelog.groovy
+++ service/grails-app/migrations/module-tenant-changelog.groovy
@@ -1,4 +1,4 @@
 databaseChangeLog = {
-  include file: 'initial-customisations.groovy'
-  include file: 'create-mod-directory.groovy'
+  include file: './initial-customisations.groovy', relativeToChangelogFile:true
+  include file: './create-mod-directory.groovy', relativeToChangelogFile:true
 }
