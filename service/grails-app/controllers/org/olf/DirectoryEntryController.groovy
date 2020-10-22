42ea9bef11c6f434786af9374b66fcddbf130283
Ian Ibbotson <ianibbo@gmail.com>
2020-10-22 10:06:44 +0100

    Demonstrate how to extend and override OkapiTenantAwareController.save and .update to add controller specific behaviour to requests for @kurtnordstrom

--- service/grails-app/controllers/org/olf/DirectoryEntryController.groovy
+++ service/grails-app/controllers/org/olf/DirectoryEntryController.groovy
@@ -5,6 +5,7 @@ import groovy.util.logging.Slf4j
 import com.k_int.okapi.OkapiTenantAwareController
 import grails.converters.JSON
 import org.olf.okapi.modules.directory.DirectoryEntry
+import com.k_int.okapi.OkapiHeaders
 
 /**
  * Access to InternalContact resources
@@ -15,6 +16,35 @@ class DirectoryEntryController extends OkapiTenantAwareController<DirectoryEntry
 
   DirectoryEntryController() {
     super(DirectoryEntry)
+  }
+
+  @Override
+  def save() {
+    DirectoryEntry.withTransaction {
+      log.debug("Overridden DirectoryEntryController::save() - called when there is a put on a directory entry resource");
+
+      // Here is one way to get hold of the permissions
+      String okapi_permissions_str = request.getHeader(OkapiHeaders.PERMISSIONS) ?: '[]'
+
+      // But the superclass should be parsing that and surfacing the permissions so that
+      // request.isUserInRole("okapi.directory.entry.item.update") 
+      // N.B. 1 The okapi. prefix which distinguishes OKAPI permissions from other spring security perms
+      // N.B. 2 You can also use the @Secured({"okapi.a.b.c"}) at the method level but the conditional nature of
+      // the requirement might mean it's cleaner to do this in the body of the method.
+
+      // Do what the superclass would have done anyway
+      super.save()
+    }
+  }
+
+
+
+  @Override
+  def update() {
+    DirectoryEntry.withTransaction {
+      log.debug("Overridden DirectoryEntryController::update() - called when there is a post on a directory entry resource");
+      super.update()
+    }
   }
 
 }
