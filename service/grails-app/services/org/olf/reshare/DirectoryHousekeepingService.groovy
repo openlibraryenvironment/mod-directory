5721c5dcf85e6f0bf6d2f43bf37b42c2b28af587
Ian Ibbotson <ianibbo@gmail.com>
2022-03-30 10:53:59 +0100

    move custom property creation to an external script - housekeeping isn't the right place

--- service/grails-app/services/org/olf/reshare/DirectoryHousekeepingService.groovy
+++ service/grails-app/services/org/olf/reshare/DirectoryHousekeepingService.groovy
@@ -63,10 +63,10 @@ class DirectoryHousekeepingService {
     final String tenant_schema_id = OkapiTenantResolver.getTenantSchemaName(tenantId)
     try {
       Tenants.withId(tenant_schema_id) {
-        ensureTextProperty('pickup_location_code', false, 'Pickup location code')
-        ensureTextProperty('delivery_stop', false, 'Delivery Stop')
-        ensureTextProperty('print_name', false, 'Print Name')
-        ensureTextProperty('key', false, 'Key')
+        // ensureTextProperty('pickup_location_code', false, 'Pickup location code')
+        // ensureTextProperty('delivery_stop', false, 'Delivery Stop')
+        // ensureTextProperty('print_name', false, 'Print Name')
+        // ensureTextProperty('key', false, 'Key')
       }
     }
     catch ( Exception e ) {
