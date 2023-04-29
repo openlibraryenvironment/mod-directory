dd1feac4ce824daed6eea7a8dc2d6977975030de
Ian Ibbotson <ianibbo@gmail.com>
2023-04-29 21:18:06 +0100

    bump commit as the 2.9.x build failed with no artefacts produced

--- service/grails-app/migrations/module-tenant-changelog.groovy
+++ service/grails-app/migrations/module-tenant-changelog.groovy
@@ -1,5 +1,8 @@
 databaseChangeLog = {
   include file: 'initial-customisations.groovy'
+  // When we are ready - doing this instead should include the migrations from dm-directory rather than having them locally
+  // include file: 'directory/create-mod-directory.groovy'
+  // When we do this we shoud also pull the settings migrations from web-toolkit rather than having them embedded here
   include file: 'create-mod-directory.groovy'
   include file: 'update-mod-directory-2-7.groovy'
 }
